009527b20fe3c5d663a1ace0e326211f125164c0
Ritik <iritikdev@gmail.com>
2026-03-10 21:35:10 +0530

    fix Error 2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**`, 
+      },
+    ],
   },
 };
 
